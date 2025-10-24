7ef28767fd1ccbbbb8f4c5df5aafc6ef1ccd420d
Daniel Alvero <dalvero589@gmail.com>
2025-10-24 23:52:04 +0700

    first commit

---
+++ bin/latihan_array_satu.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {  
+  stdout.write("Masukkan jumlah elemen array: ");
+
+  // TODO: Deklarasikan List<int> untuk arrayA, arrayB, dan hasil
+  // TODO: Deklarasikan variabel sum untuk menyimpan total penjumlahan
+
+  // TODO: Input elemen-elemen Array A
+  print("\nMasukkan elemen-elemen Array A:");
+
+  // TODO: Input elemen-elemen Array B
+  print("\nMasukkan elemen-elemen Array B:");
+
+  // TODO: Lakukan penjumlahan Array A + Array B
+
+  // TODO: Tampilkan hasil penjumlahan tiap indeks
+  print("\nHasil penjumlahan Array A + Array B adalah:");
+
+  // TODO: Hitung total (sum) dari seluruh elemen hasil
+
+  // TODO: Tampilkan hasil total penjumlahan
+
+}
