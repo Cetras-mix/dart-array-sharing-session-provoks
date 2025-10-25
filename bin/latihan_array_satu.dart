e5c9af9d9999dd76a373f4b6a5b11deef693c69e
Hylmi <hylmig7@gmail.com>
2025-10-25 12:41:24 +0700

    third commit

--- bin/latihan_array_satu.dart
+++ bin/latihan_array_satu.dart
@@ -2,22 +2,46 @@ import 'dart:io';
 
 void main() {
   stdout.write("Masukkan jumlah elemen array: ");
-  // Check heck
+  int? userInput = int.tryParse(stdin.readLineSync()?.trim() ?? '2') ?? -1;
+
   // TODO: Deklarasikan List<int> untuk arrayA, arrayB, dan hasil
+  List<int> arrayA = [];
+  List<int> arrayB = [];
+  List<int> hasilArray = [];
+
   // TODO: Deklarasikan variabel sum untuk menyimpan total penjumlahan
+  int sum = 0;
 
   // TODO: Input elemen-elemen Array A
-  print("\nMasukkan elemen-elemen Array A:");
+  for (var i = 0; i < userInput; i++) {
+    print("\nMasukkan elemen-elemen Array A:");
+    int? jumlahEarrayA =
+        int.tryParse(stdin.readLineSync()?.trim() ?? ' ') ?? -1;
+    arrayA.add(jumlahEarrayA);
+  }
 
   // TODO: Input elemen-elemen Array B
-  print("\nMasukkan elemen-elemen Array B:");
+  for (var i = 0; i < userInput; i++) {
+    print("\nMasukkan elemen-elemen Array B:");
+    int? jumlahEarrayB =
+        int.tryParse(stdin.readLineSync()?.trim() ?? ' ') ?? -1;
+    arrayB.add(jumlahEarrayB);
+  }
 
   // TODO: Lakukan penjumlahan Array A + Array B
+  for (var i = 0; i < userInput; i++) {
+    hasilArray.add(arrayA[i] + arrayB[i]);
+  }
 
   // TODO: Tampilkan hasil penjumlahan tiap indeks
-  print("\nHasil penjumlahan Array A + Array B adalah:");
+  for (var i = 0; i < userInput; i++) {
+    print("\nHasil penjumlahan Array A + Array B ($i): ${hasilArray[i]} ");
+  }
 
   // TODO: Hitung total (sum) dari seluruh elemen hasil
-
+  for (var i = 0; i < userInput; i++) {
+    sum += hasilArray[i];
+  }
   // TODO: Tampilkan hasil total penjumlahan
+  print(sum);
 }
